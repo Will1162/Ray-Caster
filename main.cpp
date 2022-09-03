d129b60c83e455cbe6fdf9212a2128822f4194a6
Will Burland <williamburland10@gmail.com>
2022-09-03 21:12:54 +0100

    Fixed distortion, added keyboard sphere movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <random>
 #include <SFML/Graphics.hpp>
 
 void DrawPixel(int x, int y, sf::Uint8 *pixels, int windowWidth, int windowHeight, unsigned char r, unsigned char g, unsigned char b)
@@ -26,7 +25,13 @@ int main()
     sprite.setTexture(texture);
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Window", sf::Style::Titlebar | sf::Style::Close);
-    
+    window.setFramerateLimit(60);
+
+    float sphereX = 0.0;
+    float sphereY = 0.0;
+    float sphereZ = 20.0;
+
+
     sf::Clock clock;
     float lastTime = 0;
     while (window.isOpen())
@@ -40,18 +45,50 @@ int main()
 
         window.clear();
 
-        float mouseX = sf::Mouse::getPosition(window).x / (float)windowWidth;
-        float mouseY = sf::Mouse::getPosition(window).y / (float)windowHeight;
+        // float mouseX = sf::Mouse::getPosition(window).x / (float)windowWidth;
+        // float mouseY = sf::Mouse::getPosition(window).y / (float)windowHeight;
+
+        float speed = 0.1;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            sphereX -= speed;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            sphereX += speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            sphereY -= speed;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            sphereY += speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            sphereZ -= speed;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+        {
+            sphereZ += speed;
+        }
+
 
         for (int i = 0; i < windowHeight; i++)
         {
             for (int j = 0; j < windowWidth; j++)
             {
                 // sphere
-                float cx = 2 * mouseX - 1;
-                float cy = 2 * mouseY - 1;
-                float cz = 1.0;
-                float r  = 0.2;
+                float cx = sphereX;
+                float cy = sphereY;
+                float cz = sphereZ;
+                float r  = 1.0;
+                
+                // float cx = 2 * mouseX - 1;
+                // float cy = 2 * mouseY - 1;
+                // float cz = 20.0;
+                // float r  = 1.0;
 
                 // camera
                 float x0 = 0.0;
@@ -61,7 +98,7 @@ int main()
                 // pixel/ray being tested
                 float x1 = (2 * j) / (float)windowWidth - 1;
                 float y1 = (2 * i) / (float)windowHeight - 1;
-                float z1 = 1.0;
+                float z1 = 5.0;
 
                 // ray vector direction
                 float dx = x1 - x0;
