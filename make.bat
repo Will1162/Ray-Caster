0a21c050f8a4e22016545ca4b25914ad0667a7b8
Will Burland <williamburland10@gmail.com>
2022-09-08 20:59:40 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -8,7 +8,7 @@ cls
 g++ main.cpp -o RayTracer-x64.exe -IC:\SFML\include -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -O2 -g -Wall -DSFML_STATIC
 
 :: run without cmd showing
-:: g++ main.cpp -o out.exe -IC:\SFML\include -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -O2 -mwindows -DSFML_STATIC
+:: g++ main.cpp -o RayTracer-x64.exe -IC:\SFML\include -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -O2 -mwindows -DSFML_STATIC
 
 if exist RayTracer-x64.exe (
 	RayTracer-x64.exe
